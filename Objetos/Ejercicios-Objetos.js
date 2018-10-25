531420bd605e0669a3e21ef2568d96ba6f023e62
elstr <eleonora.lester@gmail.com>
2018-10-25 10:18:58 -0300

    (enh): Agrega ejercicio peliculas

--- Objetos/Ejercicios-Objetos.js
+++ Objetos/Ejercicios-Objetos.js
@@ -36,3 +36,17 @@
 
 // //       y que me devuelva todas
 // //       las patentes que tiene a bordo.
+
+// // 2) Crear una funcion que agregue objetos peliculas
+// // 	•	El objeto pelicula Debera tener ID, titulo, descripcion,
+// //       año, duracion, actores, director
+// // 	•	Debera tener metodos para editar todas sus propiedades,
+// //       menos su ID, y para los actores tendra que ir agregando uno a uno
+
+// //   Crear un array de peliculas
+// // 	•	Crear una funcion que agregue una pelicula al
+// //       array de peliculas
+// // 	•	La pelicula debera tener un ID y un Titulo
+// // 	•	Crear una funcion que evalue antes de agregar que
+// //       la pelicula no fue ingresada previamente
+// // 	•	Crear una funcion que ordene las peliculas por Titulo
