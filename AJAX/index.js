0cf0148ffe346eeccdc68912f9b76b7cf5e7669b
elstr <eleonora.lester@gmail.com>
2018-11-20 20:13:04 -0300

    (enh): Posible solucion

--- AJAX/index.js
+++ AJAX/index.js
@@ -1,9 +1,24 @@
+var usuarios = [];
 function verUsuariosEnConsola() {
   $.ajax({
     url: "https://reqres.in/api/users",
     type: "GET",
     success: function(response) {
       console.log(response);
+      usuarios = response.data;
+      mostrarUsuarios();
     }
   });
+}
+
+function mostrarUsuarios() {
+  var div = $("#usuarios");
+  for (let i = 0; i < usuarios.length; i++) {
+    const usuario = usuarios[i];
+    const img = $("<img />");
+    img.attr("src", usuario.avatar);
+    const p = `<p>${usuario.first_name} ${usuario.last_name}</p>`;
+    div.append(img);
+    div.append(p);
+  }
 }
