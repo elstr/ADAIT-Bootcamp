66349a4a31df75633f376a074ee92e04346c14e4
elstr <eleonora.lester@gmail.com>
2019-01-28 16:27:57 -0300

    (enh): Agrega comentarios

--- ruteos/src/components/header/header.jsx
+++ ruteos/src/components/header/header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./header.css";
+// 1. Importo NavLink desde "react-router-dom"
 import { NavLink } from "react-router-dom";
 
 class Header extends Component {
@@ -9,17 +10,19 @@ class Header extends Component {
         <nav>
           <ul>
             <li>
+              {/* to: a donde me redirije cuando le doy click */}
+              {/* activeClassName: la clase que le pone dinamicamente cuando le doy click */}
               <NavLink exact to="/" activeClassName="is-selected">
                 Inicio
               </NavLink>
             </li>
             <li>
-              <NavLink  to="/productos" activeClassName="is-selected">
+              <NavLink to="/productos" activeClassName="is-selected">
                 Productos
               </NavLink>
             </li>
             <li>
-              <NavLink  to="/contacto" activeClassName="is-selected">
+              <NavLink to="/contacto" activeClassName="is-selected">
                 Contacto
               </NavLink>
             </li>
